124785bfc811bca2d4a93a7235332369875a1aad
MattGuy2 <matthewflammia@aol.com>
2024-03-14 11:57:13 -0400

    Created remove score and check puzzle

--- commands/admin/removescore.js
+++ commands/admin/removescore.js
@@ -1,9 +1,10 @@
-const { SlashCommandBuilder } = require('@discordjs/builders');
-const { Formatters } = require('discord.js');
+const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
+const { Wordle, Mini, Connections, Strands } = require('../../dbObjects.js');
+
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('removescore')
-		.setDescription('ADMIN ONLY removes a users score for the day')
+		.setDescription('Removes a users score')
 		.addUserOption(option =>
 			option
 				.setName('userid')
@@ -11,20 +12,45 @@ module.exports = {
 				.setRequired(true))
 		.addStringOption(option =>
 			option
+				.setName('game')
+				.setDescription('The game to check')
+				.addChoices(
+					{name:"Wordle",value:"wordle"},
+					{name:"Connections",value:"connections"},
+					{name:"Mini CW",value:"mini"},
+					{name:"Strands",value:"strands"},
+				)
+				.setRequired(true))
+		.addStringOption(option =>
+			option
 				.setName('puzzleid')
-				.setDescription('The ID of the puzzle to remove')
-				.setRequired(true)),
-	async execute(interaction, Tags) {
-		if(interaction.user.id != interaction.guild.ownerId){
-			return interaction.reply({content:`You do not have permission to use that command!`,ephemeral:true});
-		}
-		let uid = interaction.options.getUser('userid').id;
-		let pid = interaction.options.getString('puzzleid');
+				.setDescription('The ID of the puzzle to remove (use date if Mini)')
+				.setRequired(true))
+		.setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
+	async execute(interaction) {
+		const user_id = interaction.options.getUser('userid').id;
+		const game = interaction.options.getString('game');
+		const puzzle_id = interaction.options.getString('puzzleid');
 		
-		const rowcount = await Tags.destroy({where:{uid: uid, pid: pid}});
+		let rowcount = 0;
+
+		switch(game){
+			case "wordle":
+				rowcount = await Wordle.destroy({where:{user_id: user_id, server_id: interaction.guild.id, puzzle_id: puzzle_id}});
+				break;
+			case "connections":
+				rowcount = await Connections.destroy({where:{user_id: user_id, server_id: interaction.guild.id, puzzle_id: puzzle_id}});
+				break;
+			case "mini":
+				rowcount = await Mini.destroy({where:{user_id: user_id, server_id: interaction.guild.id, date: puzzle_id}});
+				break;
+			case "strands":
+				rowcount = await Strands.destroy({where:{user_id: user_id, server_id: interaction.guild.id, puzzle_id: puzzle_id}});
+				break;
+		}
 		
 		if(!rowcount)
 			return interaction.reply({content:`Could not find that puzzle for that user!`,ephemeral:true});
-		return interaction.reply({content:`Deleted puzzle ${pid} (${rowcount} row removed)!`,ephemeral:true});
+		return interaction.reply({content:`Deleted puzzle ${puzzle_id} (${rowcount} row removed)!`,ephemeral:true});
 	},
 };
--- commands/user/checkpuzzle.js
+++ commands/user/checkpuzzle.js
@@ -1,35 +1,78 @@
-const { SlashCommandBuilder } = require('@discordjs/builders');
+const { SlashCommandBuilder } = require('discord.js');
+const { Wordle, Connections, Mini, Strands } = require('../../dbObjects.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('checkpuzzle')
 		.setDescription('See a users puzzle results!')
-		.addStringOption(option => 
+		.addStringOption(option =>
 			option
-				.setName('puzzleid')
-				.setDescription('The ID of the Connections Puzzle')
+				.setName('game')
+				.setDescription('The game to check')
+				.addChoices(
+					{name:"Wordle",value:"wordle"},
+					{name:"Connections",value:"connections"},
+					{name:"Mini CW",value:"mini"},
+					{name:"Strands",value:"strands"},
+				)
 				.setRequired(true))
 		.addUserOption(option =>
 			option
 				.setName('userid')
 				.setDescription('The user to check (leave blank for yourself)')
-				.setRequired(false)),
+				.setRequired(false))
+		.addStringOption(option => 
+			option
+				.setName('puzzleid')
+				.setDescription('The ID of the puzzle (Date for Mini)')
+				.setRequired(true)),
 	async execute(interaction) {
-		let uid = interaction.options.getUser('userid');
-		if(uid == null){
-			uid = interaction.user.id
+		const user_id = interaction.options.getUser('userid') ?? interaction.user.id;
+		const puzzle_id = interaction.options.getString('puzzleid');
+		const game = interaction.options.getString('game');
+
+		switch(game){
+			case "wordle":
+				get_wordle(user_id, interaction.guild.id, puzzle_id);
+				break;
+			case "connections":
+				get_connection(user_id, interaction.guild.id, puzzle_id);
+				break;
+			case "mini":
+				get_mini(user_id, interaction.guild.id, puzzle_id);
+				break;
+			case "strands":
+				get_strand(user_id, interaction.guild.id, puzzle_id);
+				break;
 		}
-		else{
-			uid = uid.id;
-		}
-		const pid = interaction.options.getString('puzzleid');
-		
-		const tag = await Tags.findOne({where:{uid: uid, pid: pid}});
-		
-		if(tag){
-			//if puzzle is found
-			return interaction.reply({content:`Results for Connections ${pid}\n${tag.desc}`,ephemeral:true});
-		}
-		return interaction.reply({content:`Could not find Connections ${pid} for that user!`,ephemeral:true});
 	},
 };
+
+async function get_wordle(user_id, server_id, puzzle_id){
+	const result = await Wordle.findOne({where:{user_id: user_id, server_id: server_id, puzzle_id: puzzle_id}});
+	if(result){
+		return interaction.reply({content:`Puzzle ${result.puzzle_id}: Score: ${result.score}`,ephemeral:true});
+	}
+	return interaction.reply({content:`Could not find Wordle ${puzzle_id} for that user!`,ephemeral:true});
+}
+async function get_connection(user_id, server_id, puzzle_id){
+	const result = await Connections.findOne({where:{user_id: user_id, server_id: server_id, puzzle_id: puzzle_id}});
+	if(result){
+		return interaction.reply({content:`Puzzle ${result.puzzle_id}: Score: ${result.score} Guesses: ${results.guesses}`,ephemeral:true});
+	}
+	return interaction.reply({content:`Could not find Connections ${puzzle_id} for that user!`,ephemeral:true});
+}
+async function get_mini(user_id, server_id, puzzle_id){
+	const result = await Mini.findOne({where:{user_id: user_id, server_id: server_id, date: puzzle_id}});
+	if(result){
+		return interaction.reply({content:`Puzzle ${result.date}: Time: ${result.time}`,ephemeral:true});
+	}
+	return interaction.reply({content:`Could not find Mini ${puzzle_id} for that user!`,ephemeral:true});
+}
+async function get_strand(user_id, server_id, puzzle_id){
+	const result = await Strands.findOne({where:{user_id: user_id, server_id: server_id, puzzle_id: puzzle_id}});
+	if(result){
+		return interaction.reply({content:`Puzzle ${result.puzzle_id}: Theme: ${result.theme} Score: ${result.score} Hints: ${result.hints}`,ephemeral:true});
+	}
+	return interaction.reply({content:`Could not find Strand ${puzzle_id} for that user!`,ephemeral:true});
+}
